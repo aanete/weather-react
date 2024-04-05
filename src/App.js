abea8643225d9bb6ca5e6d9999b7d3a6951b7ae7
aanete <alksne.anete@gmail.com>
2024-04-05 20:15:11 +0200

    Implemented Bootstrap and css from previous Weather App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Weather from "./Weather";
+import Search from "./Search";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1>Testing</h1>
-				<Weather city="Lisbon" />
+				<Search />
 			</header>
 		</div>
 	);
---
+++ src/Search.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Search.css";
+
+export default function Search() {
+	let [city, setCity] = useState("");
+
+	let [weather, setWeather] = useState(null);
+
+	function showWeather(response) {
+		setWeather({
+			temperature: response.data.main.temp,
+			humidity: response.data.main.humidity,
+			wind: response.data.wind.speed,
+			description: response.data.weather[0].description,
+			icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+		});
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		let apiKey = `63214c4281922e3bb72fdf12dada7734`;
+		let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+		axios.get(apiUrl).then(showWeather);
+	}
+
+	function updateCity(event) {
+		setCity(event.target.value);
+	}
+
+	let form = (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="search"
+				autoFocus={true}
+				onChange={updateCity}
+				placeholder="Type a city..."
+			/>
+			<input type="submit" name="Search" />
+		</form>
+	);
+
+	return (
+		<div>
+			{form}
+			<ul>
+				<li className="currentCity">{city}</li>
+				<li>Temperature: {Math.round(weather.temperature)}°C</li>
+				<li>Humidity: {weather.humidity}%</li>
+				<li>Wind: {Math.round(weather.wind)} m/s</li>
+				<li>Description: {weather.description}</li>
+				<li>
+					<img src={weather.icon} alt="weather icon" />
+				</li>
+			</ul>
+		</div>
+	);
+}
